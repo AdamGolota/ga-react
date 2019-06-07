6b7572d7076595dce67daa599611e132780a13ff
Adam Golota <golota.adam@gmail.com>
2019-06-07 12:59:23 +0300

    add ga, remove logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactGA from 'react-ga'
 import './App.css';
+
+ReactGA.initialize('UA-123791717-1');
+ReactGA.pageview('/homepage');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Google Analytics Is Simple!
         </p>
         <a
           className="App-link"
