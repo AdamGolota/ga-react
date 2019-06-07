f5a5e062b0e698bd14825a33e8381617e979fe2d
Adam Golota <golota.adam@gmail.com>
2019-06-07 13:07:05 +0300

    change tracking code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga'
 import './App.css';
 
-ReactGA.initialize('UA-123791717-1');
+ReactGA.initialize('UA-138302548-2');
 ReactGA.pageview('/homepage');
 
 function App() {
